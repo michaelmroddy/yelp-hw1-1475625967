901440b2e12d87da543cbc52c329ac51aa29659a
Raghu Betina <raghu@firstdraft.com>
2016-10-05 00:10:01 +0000

    Direct association between reviews and review_photos

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  has_many   :review_photos,
+             :dependent => :destroy
+
   belongs_to :restaurant
 
   belongs_to :user
--- app/models/review_photo.rb
+++ app/models/review_photo.rb
@@ -1,6 +1,8 @@
 class ReviewPhoto < ApplicationRecord
   # Direct associations
 
+  belongs_to :review
+
   # Indirect associations
 
   # Validations
